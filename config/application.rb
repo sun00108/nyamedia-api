90ac87bb43a6b9e9412bbf1e41486b5c778a1e7e
sun00108 <h243sun@uwaterloo.ca>
2022-10-30 17:18:27 -0500

    fix timezone

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module NyamediaApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.time_zone = 'Beijing'
   end
 end
